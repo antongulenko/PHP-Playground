32c6269e62aed43db0cd928ef463eb3fa6bb7b7d
Anton <anton.gulenko@student.hpi.uni-potsdam.de>
2011-10-16 15:41:49 +0200

    removed tracker

--- tracker.php
+++ tracker.php
@@ -3,7 +3,8 @@ require 'php/ui/bodyTop.php' ;
 require_once 'php/user.php';
 assertLoggedIn();
 
-_iframe("php/phpbttrkplus-2.2/tracker.php");
+_h3('Hier ist noch nichts.');
+// _iframe("php/phpbttrkplus-2.2/tracker.php");
 
 require 'php/ui/bodyBottom.php';
 ?>
